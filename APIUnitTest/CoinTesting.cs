fec834ce7352da020c5b29524ea5324a9b520755
Ariel Ayala <arielayalamail@gmail.com>
2022-09-03 16:30:44 -0400

    Unit testing cases

--- APIUnitTest/CoinTesting.cs
+++ APIUnitTest/CoinTesting.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using UnitTestingAPINetCore.Controllers;
+using UnitTestingAPINetCore.Models;
 using UnitTestingAPINetCore.Services;
 
 namespace APIUnitTest
@@ -25,7 +26,33 @@ namespace APIUnitTest
 
             // Assert
             Assert.IsType<OkObjectResult>(result);
+        }
 
+        [Fact]
+        public void Get_Should_return_quantity()
+        {
+            // Act
+            var result = (OkObjectResult)_coinController.Get();
+
+            // Assert
+            var coins = Assert.IsType<List<Coin>>(result.Value);
+            Assert.True(coins.Count > 0);
+
+        }
+
+        [Fact]
+        public void GetById_Should_Be_Ok()
+        {
+            // Arrange 
+            int existingId = 1;
+
+            // Act
+            var result = (OkObjectResult)_coinController.GetById(existingId);
+
+            // Assert
+            var coin = Assert.IsType<Coin>(result?.Value);
+            Assert.True(coin != null);
+            Assert.Equal(coin?.Id, existingId);
         }
     }
 }
